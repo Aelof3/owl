60bd3b558561455c52bceb05dbc84b66b2f58a90
DUSTIN CHESSNUTT <dustinchesnutt@yahoo.com>
2020-09-29 13:03:38 -0700

    added stage

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './App.css';
 import ParticipantList from './components/ParticipantList/ParticipantList';
 import { participants } from './participants';
+import Stage from './components/Stage/Stage';
 
 function App() {
   return (
     <div className="App">
       <ParticipantList participants={participants} />
+      <Stage participants={participants}/>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Stage/Stage.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Participant from '../ParticipantList/Participant';
+
+class Stage extends React.Component {
+    
+    generateParticipants( participants ){
+        // return list of participants created from imported participants
+        let p = participants.map( participant => {
+           if( participant.onStage){ return (
+                <Participant
+                    key={btoa(`${participant.name}${participant.id}`)}
+                    id={participant.id*Math.PI}
+                    name={participant.name}
+                    avatar={participant.avatar}
+                    inSession={participant.inSession}
+                    onStage={participant.onStage}
+                />
+            );
+           };
+        } )
+
+        return p;
+    
+    }
+
+    render( ){
+        const { participants } = this.props;
+        return (
+            <ul >
+                { this.generateParticipants( participants ) }
+            </ul>
+        );
+    }
+}
+
+export default Stage;
